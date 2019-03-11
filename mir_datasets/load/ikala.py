95027ef9c8692fa7f4c3ffc8e73ecad62ca93ec7
Rachel Bittner <rachelbittner@spotify.com>
2019-03-11 18:10:26 -0400

    rename loaders to load; write initial loading functions

---
+++ mir_datasets/load/ikala.py
@@ -0,0 +1,81 @@
+"""ikala dataset loader
+"""
+from collections import namedtuple
+
+import csv
+import json
+import librosa
+import numpy as np
+import os
+
+from ..utils import abs_path
+from ..utils import F0Data, LyricsData
+
+IKALA_TIME_STEP = 0.032  # seconds
+INDEX_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "indexes/ikala_index.json")
+IKALA_INDEX = json.load(open(INDEX_PATH, 'r'))
+
+
+IKalaTrack = namedtuple(
+    'IKalaTrack',
+    ['track_id', 'f0', 'lyrics', 'audio_path',
+     'singer_id', 'song_id', 'section']
+)
+
+
+def load_ikala(data_home=None):
+    ikala_data = {}
+    for key in IKALA_INDEX.keys():
+        ikala_data[key] = load_ikala_track(key, data_home=data_home)
+    return ikala_data
+
+
+def load_ikala_track(track_id, data_home=None):
+    if track_id not in IKALA_INDEX.keys():
+        raise ValueError(
+            "{} is not a valid track ID in IKala".format(track_id))
+
+    track_data = IKALA_INDEX[track_id]
+    f0_data = load_ikala_f0(
+        abs_path(track_data['pitch_path'], data_home))
+    lyrics_data = load_ikala_lyrics(
+        abs_path(track_data['lyrics_path'], data_home))
+    return IKalaTrack(
+        track_id, f0_data, lyrics_data, track_data['audio_path'],
+        track_data['singer_id'], track_data['song_id'], track_data['section']
+    )
+
+
+def load_ikala_f0(f0_path):
+    with open(f0_path) as fhandle:
+        lines = fhandle.readlines()
+    f0_midi = np.array([float(line) for line in lines])
+    f0_hz = librosa.midi_to_hz(f0_midi) * (f0_midi > 0)
+    confidence = (f0_hz > 0).astype(int)
+    times = np.arange(len(f0_midi)) * IKALA_TIME_STEP
+    f0_data = F0Data(times, f0_hz, confidence)
+    return f0_data
+
+
+def load_ikala_lyrics(lyrics_path):
+    # input: start time (ms), end time (ms), lyric, [pronounciation]
+    with open(lyrics_path, 'r') as fhandle:
+        reader = csv.reader(fhandle, delimiter=' ')
+        start_times = []
+        end_times = []
+        lyrics = []
+        pronounciations = []
+        for line in reader:
+            start_times.append(float(line[0]) / 1000.)
+            end_times.append(float(line[1]) / 1000.)
+            lyrics.append(line[2])
+            if len(line) > 2:
+                pronounciation = ' '.join(line[3:])
+                pronounciations.append(
+                    pronounciation if pronounciation != '' else None)
+            else:
+                pronounciations.append(None)
+
+    lyrics_data = LyricsData(start_times, end_times, lyrics, pronounciations)
+    return lyrics_data
---
+++ mir_datasets/load/orchset.py
@@ -0,0 +1,65 @@
+"""Orchset Dataset Loader
+"""
+from collections import namedtuple
+
+import csv
+import json
+import numpy as np
+import os
+
+from ..utils import abs_path
+from ..utils import F0Data
+
+INDEX_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "indexes/orchset_index.json")
+ORCHSET_INDEX = json.load(open(INDEX_PATH, 'r'))
+
+OrchsetTrack = namedtuple(
+    'IKalaTrack',
+    ['track_id', 'melody', 'audio_path_mono', 'audio_path_stereo', 'composer',
+     'work', 'excerpt', 'predominant_melodic_instruments',
+     'alternating_melody', 'contains_winds', 'contains_strings',
+     'contains_brass', 'only_strings', 'only_winds', 'only_brass']
+)
+
+
+def load_orchset(data_home=None):
+    orchset_data = {}
+    for key in ORCHSET_INDEX.keys():
+        orchset_data[key] = load_orchset_track(key, data_home=data_home)
+    return orchset_data
+
+
+def load_orchset_track(track_id, data_home=None):
+    if track_id not in ORCHSET_INDEX:
+        raise ValueError(
+            "{} is not a valid track ID in Orchset".format(track_id))
+    track_data = ORCHSET_INDEX[track_id]
+    melody_data = load_orchset_melody(
+        abs_path(track_data['melody_path'], data_home))
+    mel_insts = track_data['predominant_melodic_instruments-normalized']
+    return OrchsetTrack(track_id, melody_data, track_data['audio_path_mono'],
+                        track_data['audio_path_stereo'], track_data['work'],
+                        track_data['excerpt'], mel_insts,
+                        track_data['alternating_melody'],
+                        track_data['contains_winds'],
+                        track_data['contains_strings'],
+                        track_data['contains_brass'],
+                        track_data['only_strings'], track_data['only_winds'],
+                        track_data['only_brass'])
+
+
+def load_orchset_melody(melody_path):
+    times = []
+    freqs = []
+    confidence = []
+    with open(melody_path, 'r') as fhandle:
+        reader = csv.reader(fhandle, delimiter='\t')
+        for line in reader:
+            times.append(float(line[0]))
+            freqs.append(float(line[1]))
+            confidence.append(0 if line[1] == '0' else 1)
+
+    melody_data = F0Data(
+        np.array(times), np.array(freqs), np.array(confidence))
+    return melody_data
