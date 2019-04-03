4bbfdd7569b8c45b4c4bfea14b884c387ea24973
Rachel Bittner <rmb456@nyu.edu>
2019-04-03 15:07:05 -0400

    Ikala audio helpers (#15)
    * add ikala audio helper functions
    
    * add audio filepaths helper function
    
    * remove audio list helpers, add instructions to .download()

--- mir_dataset_loaders/ikala.py
+++ mir_dataset_loaders/ikala.py
@@ -10,7 +10,7 @@ import os
 from urllib import request
 
 from . import IKALA_INDEX_PATH
-from .utils import get_local_path, validator, F0Data, LyricsData
+from .utils import get_save_path, get_local_path, validator, F0Data, LyricsData
 
 
 IKALA_TIME_STEP = 0.032  # seconds
@@ -32,9 +32,18 @@ IKalaTrack = namedtuple(
 )
 
 
-def download():
-    raise NotImplementedError(
-        "Unfortunately the iKala dataset is not available for download.")
+def download(data_home=None):
+    save_path = get_save_path(data_home)
+    print("""
+        Unfortunately the iKala dataset is not available for download.
+        If you have the iKala dataset, place the contents into a folder called
+        iKala with the following structure:
+            > iKala/
+                > Lyrics/
+                > PitchLabel/
+                > Wavfile/
+        and copy the iKala folder to {}
+    """.format(save_path))
 
 
 def validate(data_home=None):
@@ -82,7 +91,30 @@ def load_track(track_id, data_home=None):
     )
 
 
+def load_ikala_vocal_audio(ikalatrack):
+    audio_path = ikalatrack.audio_path
+    audio, sr = librosa.load(audio_path, sr=None, mono=False)
+    vocal_channel = audio[1, :]
+    return vocal_channel, sr
+
+
+def load_ikala_instrumental_audio(ikalatrack):
+    audio_path = ikalatrack.audio_path
+    audio, sr = librosa.load(audio_path, sr=None, mono=False)
+    instrumental_channel = audio[0, :]
+    return instrumental_channel, sr
+
+
+def load_ikala_mix_audio(ikalatrack):
+    audio_path = ikalatrack.audio_path
+    mixed_audio, sr = librosa.load(audio_path, sr=None, mono=True)
+    return mixed_audio, sr
+
+
 def _load_f0(f0_path):
+    if not os.path.exists(f0_path):
+        return None
+
     with open(f0_path) as fhandle:
         lines = fhandle.readlines()
     f0_midi = np.array([float(line) for line in lines])
@@ -94,6 +126,8 @@ def _load_f0(f0_path):
 
 
 def _load_lyrics(lyrics_path):
+    if not os.path.exits(lyrics_path):
+        return None
     # input: start time (ms), end time (ms), lyric, [pronounciation]
     with open(lyrics_path, 'r') as fhandle:
         reader = csv.reader(fhandle, delimiter=' ')
