a6bc35023f219ea75682827d95a1189ddae53680
Magdalena Fuentes <mgfuenteslujambio@gmail.com>
2019-06-27 19:55:54 +0200

    add rwc_classical

--- scripts/make_rwc_classical_index.py
+++ scripts/make_rwc_classical_index.py
@@ -3,7 +3,7 @@ import argparse
 import hashlib
 import json
 import os
-
+import csv
 
 RWC_CLASSICAL_INDEX_PATH = "../mirdata/indexes/rwc_classical_index.json"
 
@@ -43,13 +43,13 @@ def make_rwc_classical_index(data_path):
         suffix = []
         track = []
         for line in reader:
-            if not line[0]=="Piece No.":
+            if not line[0] == "Piece No.":
                 p = '00' + line[0].split('.')[1][1:]
                 piece.append(p[len(p)-3:])
                 suffix.append(line[1][1:])
                 track.append(line[2][-2:])
 
-    mapping_track = {p: t for p,t in zip(piece, track)}
+    mapping_track = {p: t for p, t in zip(piece, track)}
     mapping_folder = {p: s for p, s in zip(piece, suffix)}
 
     track_ids = sorted(
@@ -59,9 +59,9 @@ def make_rwc_classical_index(data_path):
     rwc_classical_index = {}
     for track_id in track_ids:
         # audio
-        audio_folder = 'rwc-c-m{}'.format(mapping_folder[track_id[4:]])
+        audio_folder = 'rwc-c-m{}'.format(mapping_folder[track_id[4:7]])
         audio_path = os.path.join(audio_dir, audio_folder)
-        audio_track = str(int(mapping_track[track_id[4:]]))
+        audio_track = str(int(mapping_track[track_id[4:7]]))
         audio_checksum = md5(os.path.join(audio_path,
                                           "{}.wav".format(audio_track)))
         annot_checksum = []
@@ -71,14 +71,16 @@ def make_rwc_classical_index(data_path):
             if os.path.exists(os.path.join(annotations_dir,
                                            'AIST.RWC-MDB-C-2001.{}'.format(f), '{}.{}.TXT'.format(track_id, f))):
                 annot_checksum.append(md5(os.path.join(annotations_dir,
-                                           'AIST.RWC-MDB-C-2001.{}'.format(f), '{}.{}.TXT'.format(track_id, f))))
+                                                       'AIST.RWC-MDB-C-2001.{}'.format(f),
+                                                       '{}.{}.TXT'.format(track_id, f))))
                 annot_rels.append(os.path.join('RWC-Jazz', 'annotations',
-                                           'AIST.RWC-MDB-C-2001.{}'.format(f), '{}.{}.TXT'.format(track_id, f)))
+                                               'AIST.RWC-MDB-C-2001.{}'.format(f),
+                                               '{}.{}.TXT'.format(track_id, f)))
             else:
                 annot_checksum.append(None)
                 annot_rels.append(None)
 
-        rwc_classical_index[track_id] = {
+        rwc_classical_index[track_id[:7]] = {
             'audio': (
                 os.path.join('RWC-Classical', 'audio', audio_folder,
                              "{}.wav".format(audio_track)),
@@ -110,4 +112,3 @@ if __name__ == "__main__":
                         help="Path to RWC-Classical data folder.")
 
     main(PARSER.parse_args())
-
