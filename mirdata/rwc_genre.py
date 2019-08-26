caecde4a65ee25cd6f0885bfdbad97d00583f129
Rachel Bittner <rachelbittner@spotify.com>
2019-08-26 16:40:17 +0200

    update RemoteFileMetadata

--- mirdata/rwc_genre.py
+++ mirdata/rwc_genre.py
@@ -18,17 +18,20 @@ METADATA_REMOTE = download_utils.RemoteFileMetadata(
     filename='rwc-g.csv',
     url='https://github.com/magdalenafuentes/metadata/archive/master.zip',
     checksum='7dbe87fedbaaa1f348625a2af1d78030',
+    destination_dir=None,
 )
 DATASET_DIR = 'RWC-Genre'
 ANNOTATIONS_REMOTE_1 = download_utils.RemoteFileMetadata(
     filename='AIST.RWC-MDB-G-2001.BEAT.zip',
     url='https://staff.aist.go.jp/m.goto/RWC-MDB/AIST-Annotation/AIST.RWC-MDB-G-2001.BEAT.zip',
     checksum='66427ce5f4485088c6d9bc5f7394f65f',
+    destination_dir='annotations',
 )
 ANNOTATIONS_REMOTE_2 = download_utils.RemoteFileMetadata(
     filename='AIST.RWC-MDB-G-2001.CHORUS.zip',
     url='https://staff.aist.go.jp/m.goto/RWC-MDB/AIST-Annotation/AIST.RWC-MDB-G-2001.CHORUS.zip',
     checksum='e9fe612a0ddc7a83f3c1d17fb5fec32a',
+    destination_dir='annotations',
 )
 
 
@@ -118,14 +121,6 @@ def download(data_home=None, force_overwrite=False):
     if data_home is None:
         data_home = utils.get_default_dataset_path(DATASET_DIR)
 
-    annotations_path = os.path.join(data_home, 'annotations')
-
-    download_utils.downloader(
-        annotations_path,
-        force_overwrite=force_overwrite,
-        zip_downloads=[ANNOTATIONS_REMOTE_1, ANNOTATIONS_REMOTE_2],
-    )
-
     info_message = """
         Unfortunately the audio files of the RWC-Genre dataset are not available
         for download. If you have the RWC-Genre dataset, place the contents into a
@@ -141,7 +136,7 @@ def download(data_home=None, force_overwrite=False):
 
     download_utils.downloader(
         data_home,
-        zip_downloads=[METADATA_REMOTE],
+        zip_downloads=[METADATA_REMOTE, ANNOTATIONS_REMOTE_1, ANNOTATIONS_REMOTE_2],
         info_message=info_message,
         force_overwrite=force_overwrite,
     )
--- tests/test_guitarset.py
+++ tests/test_guitarset.py
@@ -19,7 +19,7 @@ def test_track_basic():
 
     with pytest.raises(ValueError):
         guitarset.Track('asdfasdf', data_home=TEST_DATA_HOME)
-    
+
     # test __repr__
     assert isinstance(track_default.__repr__(), str)
 
@@ -37,7 +37,7 @@ def test_track_simple_attributes():
     assert TRACK.tempo == 119
     assert TRACK.mode == 'solo'
     assert TRACK.style == 'Bossa Nova'
-    
+
 
 def test_track_cached_anno():
     # test that cached properties don't fail and have the expected type
@@ -52,8 +52,7 @@ def test_track_cached_anno():
 
 
 def test_load_beats():
-    assert np.allclose(TRACK.beats.beat_times, 
-                       [0.50420168, 1.00840336, 1.51260504])
+    assert np.allclose(TRACK.beats.beat_times, [0.50420168, 1.00840336, 1.51260504])
     assert np.allclose(TRACK.beats.beat_positions, [2, 3, 4])
 
 
@@ -70,45 +69,55 @@ def test_load_chords():
 def test_load_keys():
     assert np.allclose(TRACK.key_mode.start_times, [0])
     assert np.allclose(TRACK.key_mode.end_times, [2])
-    assert TRACK.key_mode.keys == ['G:major']  
+    assert TRACK.key_mode.keys == ['G:major']
 
 
 def test_load_contours():
-    assert np.allclose(TRACK.pitch_contours['e'].times[:10],
-                       [0.7670358269999724,
-                        0.7728408159999844,
-                        0.778645804000007,
-                        0.7844507929999054,
-                        0.7902557819999174,
-                        0.79606076999994,
-                        0.801865758999952,
-                        0.8076707479999641,
-                        0.8134757359999867,
-                        0.8192807249999987])
-    assert np.allclose(TRACK.pitch_contours['e'].frequencies[:10],
-                       [393.388,
-                        393.301,
-                        393.386,
-                        393.348,
-                        393.377,
-                        393.389,
-                        393.389,
-                        393.351,
-                        393.352,
-                        393.37])
-    assert np.allclose(TRACK.pitch_contours['e'].confidence[:10],
-                       np.ones((10,)))
+    assert np.allclose(
+        TRACK.pitch_contours['e'].times[:10],
+        [
+            0.7670358269999724,
+            0.7728408159999844,
+            0.778645804000007,
+            0.7844507929999054,
+            0.7902557819999174,
+            0.79606076999994,
+            0.801865758999952,
+            0.8076707479999641,
+            0.8134757359999867,
+            0.8192807249999987,
+        ],
+    )
+    assert np.allclose(
+        TRACK.pitch_contours['e'].frequencies[:10],
+        [
+            393.388,
+            393.301,
+            393.386,
+            393.348,
+            393.377,
+            393.389,
+            393.389,
+            393.351,
+            393.352,
+            393.37,
+        ],
+    )
+    assert np.allclose(TRACK.pitch_contours['e'].confidence[:10], np.ones((10,)))
 
 
 def test_load_notes():
-    assert np.allclose(TRACK.notes['e'].start_times,
-                       [0.7612308390022235, 1.5072852607709137, 1.7806185941042258])
-    assert np.allclose(TRACK.notes['e'].end_times,
-                       [1.2604598639455844, 1.7336798185940552, 2.0])
-    assert np.allclose(TRACK.notes['e'].notes,
-                       [67.0576287044242, 71.03221526299762, 71.03297250121584])
-    assert np.allclose(TRACK.notes['e'].confidence,
-                       [1, 1, 1])
+    assert np.allclose(
+        TRACK.notes['e'].start_times,
+        [0.7612308390022235, 1.5072852607709137, 1.7806185941042258],
+    )
+    assert np.allclose(
+        TRACK.notes['e'].end_times, [1.2604598639455844, 1.7336798185940552, 2.0]
+    )
+    assert np.allclose(
+        TRACK.notes['e'].notes, [67.0576287044242, 71.03221526299762, 71.03297250121584]
+    )
+    assert np.allclose(TRACK.notes['e'].confidence, [1, 1, 1])
 
 
 def test_audio_mono():
