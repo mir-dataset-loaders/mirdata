ac2258e770efa9073e08390cadb5e710e23f2b70
Magdalena Fuentes <mgfuenteslujambio@gmail.com>
2019-08-30 16:16:34 +0200

    fix checksum in beatles and salami, simplify tar.gz downloader

--- mirdata/download_utils.py
+++ mirdata/download_utils.py
@@ -220,10 +220,10 @@ def untar(tar_path, cleanup=False):
     cleanup: bool, default=False
         If True, remove tarfile after untarring.
     """
-    if tar_path.endswith('tar.gz'):
-        tfile = tarfile.open(tar_path, 'r:gz')
-    else:
-        tfile = tarfile.TarFile(tar_path, 'r')
+    # if tar_path.endswith('tar.gz'):
+    #    tfile = tarfile.open(tar_path, 'r:gz')
+    # else:
+    tfile = tarfile.open(tar_path, 'r')
     tfile.extractall(os.path.dirname(tar_path))
     tfile.close()
     if cleanup:
