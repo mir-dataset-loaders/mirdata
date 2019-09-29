2fc52f6de210eae5ba220bf8b054cfa641215327
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2019-09-29 13:14:25 -0400

    test_load in MSDB

--- tests/test_medley_solos_db.py
+++ tests/test_medley_solos_db.py
@@ -7,6 +7,13 @@ import pytest
 
 from mirdata import medley_solos_db, utils
 
+def test_load():
+    data_home = 'tests/resources/mir_datasets/Medley-solos-DB'
+    msdb_data = orchset.load(data_home=data_home, silence_validator=True)
+    assert type(msdb_data) is dict
+    assert len(msdb_data.keys()) == 1
+
+
 def test_cite():
     cite_str = orchset.cite()
     assert len(cite_str) > 0
