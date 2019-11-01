16d988e70b6efd774c22c1b6fd53dd2a4dd78fe5
Magdalena Fuentes <mgfuenteslujambio@gmail.com>
2019-11-01 20:49:57 +0100

    some tests to_jams

--- tests/test_beatles.py
+++ tests/test_beatles.py
@@ -77,6 +77,40 @@ def test_track():
     assert track.key == None
 
 
+def test_to_jams():
+
+    data_home = 'tests/resources/mir_datasets/Beatles'
+    track = beatles.Track('0111', data_home=data_home)
+    jam = track.to_jams()
+
+    beats = jam.search(namespace='beat')[0]['data']
+    assert [beat.time for beat in beats] == [13.249, 13.959, 14.416, 14.965, 15.453, 15.929, 16.428]
+    assert [beat.duration for beat in beats] == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    assert [beat.value for beat in beats] == [2, 3, 4, 1, 2, 3, 4]
+    assert [beat.confidence for beat in beats] == [None, None, None, None, None, None, None]
+
+    segments = jam.search(namespace='segment')[0]['data']
+    assert [segment.time for segment in segments] == [0.0, 0.465]
+    assert [segment.duration for segment in segments] == [0.465, 14.466]
+    assert [segment.value for segment in segments] == ['silence', 'intro']
+    assert [segment.confidence for segment in segments] == [None, None]
+
+    chords = jam.search(namespace='chord')[0]['data']
+    assert [chord.time for chord in chords] == [0.0, 4.586464, 6.98973]
+    assert [chord.duration for chord in chords]== [0.497838, 2.4032659999999995, 2.995374]
+    assert [chord.value for chord in chords] == ['N', 'E:min', 'G']
+    assert [chord.confidence for chord in chords]== [None, None, None]
+
+    keys = jam.search(namespace='key')[0]['data']
+    assert [key.time for key in keys]== [0.0]
+    assert [key.duration for key in keys] == [119.333]
+    assert [key.value for key in keys] == ['E']
+    assert [key.confidence for key in keys] == [None]
+
+    assert jam['file_metadata']['title'] == '11_-_Do_You_Want_To_Know_A_Secret'
+    assert jam['file_metadata']['artist'] == 'The Beatles'
+
+
 def test_track_ids():
     track_ids = beatles.track_ids()
     assert type(track_ids) is list
--- tests/test_rwc_jazz.py
+++ tests/test_rwc_jazz.py
@@ -68,6 +68,31 @@ def test_track():
     )
     assert track.__repr__() == repr_string
 
+    print(track.to_jams())
+
+
+def test_to_jams():
+
+    data_home = 'tests/resources/mir_datasets/RWC-Jazz'
+    track = rwc_jazz.Track('RM-J004', data_home=data_home)
+    jam = track.to_jams()
+
+    beats = jam.search(namespace='beat')[0]['data']
+    print([beat.time for beat in beats])
+    assert [beat.time for beat in beats] == [0.05, 0.86, 1.67, 2.48, 3.29, 4.1, 4.91, 5.72, 6.53, 7.34]
+    assert [beat.duration for beat in beats] == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    assert [beat.value for beat in beats] == [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+    assert [beat.confidence for beat in beats] == [None, None, None, None, None, None, None, None, None, None]
+
+    segments = jam.search(namespace='segment')[0]['data']
+    assert [segment.time for segment in segments] == [0.05, 6.53, 152.06]
+    assert [segment.duration for segment in segments] == [6.48, 13.099999999999998, 13.319999999999993]
+    assert [segment.value for segment in segments] == ['nothing', 'chorus A', 'chorus B']
+    assert [segment.confidence for segment in segments] == [None, None, None]
+
+    assert jam['file_metadata']['title'] == 'Crescent Serenade (Piano Solo)'
+    assert jam['file_metadata']['artist'] == 'Makoto Nakamura'
+
 
 def test_track_ids():
     track_ids = rwc_jazz.track_ids()
@@ -99,6 +124,16 @@ def test_load_metadata():
         'duration': 167,
         'variation': 'Instrumentation 1',
         'instruments': 'Pf',
+    }
+    assert metadata['RM-J044'] == {
+        'piece_number': 'No. 44',
+        'suffix': 'M04',
+        'track_number': 'Tr. 09',
+        'title': 'Joyful, Joyful, We Adore Thee',
+        'artist': 'K’s Band',
+        'duration': 270,
+        'variation': 'Style (Free jazz)',
+        'instruments': 'Pf & Bs & Dr & Gt & Ts & Fl & Bar',
     }
 
     metadata_none = rwc_jazz._load_metadata('asdf/asdf')
