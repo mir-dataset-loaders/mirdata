19f00606c820f4b7d83da97109584f8687f2665e
Rachel Bittner <rachelbittner@spotify.com>
2019-11-22 14:17:54 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
     setup(
         name='mirdata',
-        version='0.1.0',
+        version='0.1.1',
         description='Common loaders for MIR datasets.',
         url='https://github.com/mir-dataset-loaders/mirdata',
         packages=find_packages(exclude=['test', '*.test', '*.test.*']),
