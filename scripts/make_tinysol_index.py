892d38d928641ed5746b8ebc89dcc64a2bf5fe95
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2020-01-28 21:31:31 -0500

    write make_tinysol_index

---
+++ scripts/make_tinysol_index.py
@@ -0,0 +1,69 @@
+import argparse
+import csv
+import glob
+import hashlib
+import json
+import os
+
+
+TINYSOL_INDEX_PATH = '../mirdata/indexes/tinysol_index.json'
+
+
+def md5(file_path):
+    """Get md5 hash of a file.
+
+    Parameters
+    ----------
+    file_path: str
+        File path.
+
+    Returns
+    -------
+    md5_hash: str
+        md5 hash of data in file_path
+    """
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as fhandle:
+        for chunk in iter(lambda: fhandle.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def make_tinysol_index(tinysol_data_path):
+    tinysol_index = {}
+
+    anno_path = os.path.join(tinysol_data_path, "TinySOL_metadata.csv")
+    audio_dir = os.path.join(tinysol_data_path, "audio")
+
+    with open(anno_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        for row in csv_reader:
+            wav_name = row[0]
+            family_str = row[1]
+            instr_str = row[2]
+            local_path = os.path.join(family_str, instr_str, wav_name + ".wav")
+            audio_path = os.path.join(audio_dir, local_path)
+            audio_checksum = md5(audio_path)
+            tinysol_index[wav_name] = {
+                "audio": (
+                    local_path,
+                    audio_checksum
+                ),
+            }
+
+    with open(TINYSOL_INDEX_PATH, 'w') as fhandle:
+        json.dump(tinysol_index, fhandle, indent=2)
+
+
+def main(args):
+    make_tinysol_index(args.tinysol_data_path)
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(description='Make TinySOL index file.')
+    PARSER.add_argument(
+        'tinysol_data_path', type=str, help='Path to TinySOL data folder.'
+    )
+
+    main(PARSER.parse_args())
