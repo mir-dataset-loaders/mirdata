cb5a98f7a953820e8161e0fc6fe8adb866540de3
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2020-02-24 18:40:49 -0500

    bugfix orchideasol tests

--- tests/test_orchideasol.py
+++ tests/test_orchideasol.py
@@ -6,14 +6,14 @@ import os
 
 import pytest
 
-from mirdata import tinysol, utils
+from mirdata import orchideasol, utils
 from tests.test_utils import DEFAULT_DATA_HOME
 from tests.test_download_utils import mock_downloader
 
 
 def test_track():
     # test data home None
-    track_default = tinysol.Track('Fl-jet_wh-N-N-N-N')
+    track_default = orchideasol.Track('Fl-jet_wh-N-N-N-N')
     assert track_default._data_home == os.path.join(DEFAULT_DATA_HOME, 'OrchideaSOL')
 
     # test with custom data_home
@@ -21,21 +21,21 @@ def test_track():
 
     # missing track
     with pytest.raises(ValueError):
-        tinysol.Track('asdfasdf', data_home=data_home)
+        orchideasol.Track('asdfasdf', data_home=data_home)
 
     # test with a wind instrument
-    track = tinysol.Track('Fl-jet_wh-N-N-N-N', data_home=data_home)
+    track = orchideasol.Track('Fl-jet_wh-N-N-N-N', data_home=data_home)
     assert track.track_id == 'Fl-jet_wh-N-N-N-N'
     assert track._data_home == data_home
     y, sr = track.audio
     assert y.shape == (51204,)
     assert sr == 22050
-    repr_string = 'TinySOL Track(instrument=Flute, mute=N, technique=jet_whistle, pitch=N, dynamics=N)'
+    repr_string = 'OrchideaSOL Track(instrument=Flute, mute=N, technique=jet_whistle, pitch=N, dynamics=N)'
     assert track.__repr__() == repr_string
 
     # test with a string instrument
-    track = tinysol.Track('Cb+S-trem-A2-mf-1c-N', data_home=data_home)
-    repr_string = 'TinySOL Track(instrument=Contrabass, mute=sordina, technique=tremolo, pitch=A2, dynamics=mf)'
+    track = orchideasol.Track('Cb+S-trem-A2-mf-1c-N', data_home=data_home)
+    repr_string = 'OrchideaSOL Track(instrument=Contrabass, mute=sordina, technique=tremolo, pitch=A2, dynamics=mf)'
     assert track.__repr__() == repr_string
 
 
@@ -43,7 +43,7 @@ def test_to_jams():
     data_home = 'tests/resources/mir_datasets/OrchideaSOL'
 
     # Case with a wind instrument (no string_id)
-    track = tinysol.Track('Fl-jet_wh-N-N-N-N', data_home=data_home)
+    track = orchideasol.Track('Fl-jet_wh-N-N-N-N', data_home=data_home)
     jam = track.to_jams()
 
     assert jam['sandbox']['Fold'] == 2
@@ -63,7 +63,7 @@ def test_to_jams():
     assert jam['sandbox']['Resampled']
 
     # Case with a string instrument
-    track = tinysol.Track('Cb+S-trem-A2-mf-1c-N', data_home=data_home)
+    track = orchideasol.Track('Cb+S-trem-A2-mf-1c-N', data_home=data_home)
     jam = track.to_jams()
 
     assert jam['sandbox']['Fold'] == 2
@@ -84,31 +84,31 @@ def test_to_jams():
 
 
 def test_track_ids():
-    track_ids = tinysol.track_ids()
+    track_ids = orchideasol.track_ids()
     assert type(track_ids) is list
     assert len(track_ids) == 13265
 
 
 def test_load():
     data_home = 'tests/resources/mir_datasets/OrchideaSOL'
-    tinysol_data = tinysol.load(data_home=data_home)
-    assert type(tinysol_data) is dict
-    assert len(tinysol_data.keys()) == 13265
+    orchideasol_data = orchideasol.load(data_home=data_home)
+    assert type(orchideasol_data) is dict
+    assert len(orchideasol_data.keys()) == 13265
 
-    tinysol_data = tinysol.load()
-    assert type(tinysol_data) is dict
-    assert len(tinysol_data.keys()) == 13265
+    orchideasol_data = orchideasol.load()
+    assert type(orchideasol_data) is dict
+    assert len(orchideasol_data.keys()) == 13265
 
 
 def test_download(mock_downloader):
-    tinysol.download()
+    orchideasol.download()
     mock_downloader.assert_called()
 
 
 def test_validate():
-    tinysol.validate()
-    tinysol.validate(silence=True)
+    orchideasol.validate()
+    orchideasol.validate(silence=True)
 
 
 def test_cite():
-    cite_str = tinysol.cite()
+    cite_str = orchideasol.cite()
