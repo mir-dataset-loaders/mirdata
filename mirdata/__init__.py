93259f735cd26d33a2923ee756de6a957c746509
Rachel Bittner <rachelbittner@spotify.com>
2020-03-05 19:19:59 -0500

    add __all__

--- mirdata/__init__.py
+++ mirdata/__init__.py
@@ -4,15 +4,20 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from . import beatles
-from . import ikala
-from . import medleydb_melody
-from . import medleydb_pitch
-from . import medley_solos_db
-from . import orchset
-from . import rwc_classical
-from . import rwc_jazz
-from . import rwc_popular
-from . import salami
-from . import guitarset
-from . import gtzan_genre
+__all__ = [
+    'beatles',
+    'dali',
+    'gtzan_genre',
+    'guitarset',
+    'ikala',
+    'medley_solos_db',
+    'medleydb_melody',
+    'medleydb_pitch',
+    'orchset',
+    'rwc_classical',
+    'rwc_jazz',
+    'rwc_classical',
+    'salami'
+]
+
+from . import *
