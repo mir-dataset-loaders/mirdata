1bb193ae452ce3ed8fe04cad982080ddda09d13d
Marius Miron <miron.marius@gmail.com>
2020-09-22 11:45:25 +0200

    test phenicx_anechoic solely with beethoven

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -20,6 +20,7 @@ CUSTOM_TEST_TRACKS = {
     'guitarset': '03_BN3-119-G_solo',
     'medley_solos_db': 'd07b1fc0-567d-52c2-fef4-239f31c9d40e',
     'medleydb_melody': 'MusicDelta_Beethoven',
+    'phenicx_anechoic': 'beethoven',
     'rwc_classical': 'RM-C003',
     'rwc_jazz': 'RM-J004',
     'rwc_popular': 'RM-P001',
