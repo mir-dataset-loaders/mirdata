f755afaffae5c3b0081c5ae9647fa02cbb35850a
pedroramonedafranco <pppramonedafranco@hotmail.com>
2020-10-09 14:04:01 +0200

    trying @magdalena suggestions

--- tests/test_giantsteps_key.py
+++ tests/test_giantsteps_key.py
@@ -8,15 +8,15 @@ from tests.test_utils import run_track_tests
 
 def test_track():
     default_trackid = '3'
-    data_home = 'tests/resources/mir_datasets/giantsteps_key'
+    data_home = 'tests/resources/mir_datasets/GiantSteps_key'
     track = giantsteps_key.Track(default_trackid, data_home=data_home)
 
     expected_attributes = {
-        'audio_path': 'tests/resources/mir_datasets/giantsteps_key/audio/10089 Jason Sparks - Close My Eyes feat. J. '
+        'audio_path': 'tests/resources/mir_datasets/GiantSteps_key/audio/10089 Jason Sparks - Close My Eyes feat. J. '
                       'Little (Original Mix).mp3',
-        'keys_path': 'tests/resources/mir_datasets/giantsteps_key/keys_gs+/10089 Jason Sparks - Close My Eyes feat. J. '
+        'keys_path': 'tests/resources/mir_datasets/GiantSteps_key/keys_gs+/10089 Jason Sparks - Close My Eyes feat. J. '
                      'Little (Original Mix).txt',
-        'metadata_path': 'tests/resources/mir_datasets/giantsteps_key/meta/10089 Jason Sparks - Close My Eyes feat. J. '
+        'metadata_path': 'tests/resources/mir_datasets/GiantSteps_key/meta/10089 Jason Sparks - Close My Eyes feat. J. '
                          'Little (Original Mix).json',
         'title': '10089 Jason Sparks - Close My Eyes feat. J. Little (Original Mix)',
         'track_id': '3',
@@ -39,7 +39,7 @@ def test_track():
 
 
 def test_to_jams():
-    data_home = 'tests/resources/mir_datasets/giantsteps_key'
+    data_home = 'tests/resources/mir_datasets/GiantSteps_key'
     track = giantsteps_key.Track('3', data_home=data_home)
     jam = track.to_jams()
     assert (
@@ -69,7 +69,7 @@ def test_to_jams():
 
 def test_load_key():
     key_path = (
-            'tests/resources/mir_datasets/giantsteps_key/keys_gs+/10089 Jason Sparks - Close My Eyes feat. J. ' +
+            'tests/resources/mir_datasets/GiantSteps_key/keys_gs+/10089 Jason Sparks - Close My Eyes feat. J. ' +
             'Little (Original Mix).txt'
     )
     key_data = giantsteps_key.load_key(key_path)
@@ -83,7 +83,7 @@ def test_load_key():
 
 def test_load_meta():
     meta_path = (
-            'tests/resources/mir_datasets/giantsteps_key/meta/10089 Jason Sparks - Close My Eyes feat. J. ' +
+            'tests/resources/mir_datasets/GiantSteps_key/meta/10089 Jason Sparks - Close My Eyes feat. J. ' +
             'Little (Original Mix).json'
     )
     genres = {'genres': ['Breaks'], 'sub_genres': []}
