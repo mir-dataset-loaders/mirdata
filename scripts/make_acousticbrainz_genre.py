d0840e70388428a237a8c255aa6dbbd7f634dffe
pedroramonedafranco <pppramonedafranco@hotmail.com>
2020-10-28 12:58:54 +0100

    first prototype loader

--- scripts/make_acousticbrainz_genre.py
+++ scripts/make_acousticbrainz_genre.py
@@ -7,7 +7,7 @@ import itertools
 import re
 
 
-acousticbrainz_genre_INDEX_PATH = '../mirdata/indexes/acousticbrainz_genre_index.json'
+acousticbrainz_genre_INDEX_PATH = '../mirdata/indexes/test_acousticbrainz_genre_index.json'
 ACOUSTICBRAINZ_GENRE_ANNOTATION_SCHEMA = ['JAMS']
 
 
@@ -30,9 +30,8 @@ def md5(file_path):
 
 
 def make_acousticbrainz_genre_index(data_path):
-    count = 0
-    acousticbrainz_genre_index = {}
-    datasets = ['tagtraum']  # ,'allmusic', 'lastfm', 'tagtraum', 'discogs']
+    index = 0
+    datasets = ['tagtraum', 'allmusic', 'lastfm', 'discogs']
     dataset_types = ['validation', 'train']
     f = open(acousticbrainz_genre_INDEX_PATH, 'w')
     f.write('{\n')
@@ -40,7 +39,8 @@ def make_acousticbrainz_genre_index(data_path):
         tsv_file = open(os.path.join(data_path, "acousticbrainz-mediaeval-" + dataset + "-" + dataset_type + ".tsv"))
         read_tsv = csv.reader(tsv_file, delimiter="\t")
         next(read_tsv, None)
-        for row in read_tsv:
+        read_tsv_list = list(read_tsv)
+        for line, row in enumerate(read_tsv_list):
             mbid = ""
             track_id = dataset + '#' + dataset_type
             for i, r in enumerate(row):
@@ -48,13 +48,18 @@ def make_acousticbrainz_genre_index(data_path):
                 if i == 0:
                     mbid = r
             ann_path = os.path.join(data_path, "acousticbrainz-mediaeval-" + dataset_type, mbid[:2], mbid + ".json")
-            f.write('\t\"%s\": {\n' % (track_id,))
-            f.write('\t\t\"annotations\": [\n')
-            f.write('\t\t\t\"%s\",\n' % (ann_path.replace(data_path + '/', ''),))
-            f.write('\t\t\t\"%s\"\n' % md5(ann_path))
-            f.write('\t\t\"]\n')
-            f.write('\t},\n')
-            count += 1
+            f.write('  \"%s\": {\n' % (track_id,))
+            f.write('    \"data\": [\n')
+            f.write('      \"%s\",\n' % (ann_path.replace(data_path + '/', ''),))
+            f.write('      \"%s\"\n' % md5(ann_path))
+            f.write('    ]\n')
+            is_the_last = dataset == datasets[-1] and dataset_type == dataset_types[-1] and line == len(read_tsv_list)-1
+            if not is_the_last:
+                f.write('  },\n')
+            else:
+                f.write('  }\n')
+            index += 1
+
     f.write('}')
 
 
@@ -66,4 +71,8 @@ if __name__ == '__main__':
     PARSER = argparse.ArgumentParser(description='Make acousticbrainz_genre index file.')
     PARSER.add_argument('acousticbrainz_genre_data_path', type=str, help='Path to acousticbrainz_genre data folder.')
     main(PARSER.parse_args())
+    # with open(acousticbrainz_genre_INDEX_PATH, 'r') as json_file:
+    #     data = json.load(json_file)
+    #     for row in data:
+    #         print(row)
 
