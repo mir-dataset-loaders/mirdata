4c00e4ea9e14e54039159a30b57ce212507ac1d7
Genís Plaja <45232996+genisplaja@users.noreply.github.com>
2020-10-30 00:30:31 +0100

    Adding loader for Mridangam Stroke Dataset (#285)
    * Mridangam Stroke Dataset Loader
    
    * Mridangam testing resources
    
    * Update documentation
    
    * Update README.md
    
    * Add Mridangam to test_loaders
    
    * Mridangam Stroke Dataset Loader
    
    * Mridangam testing resources
    
    * Update documentation
    
    * Update README.md
    
    * Add Mridangam to test_loaders
    
    * Remove spectrum from Mridangam
    
    * Update documentation
    
    * Fix ann loaders and add dictionary checking
    
    Co-authored-by: Pedro <pppramonedafranco@hotmail.com>

--- tests/test_giantsteps_key.py
+++ tests/test_giantsteps_key.py
@@ -13,11 +13,11 @@ def test_track():
 
     expected_attributes = {
         'audio_path': 'tests/resources/mir_datasets/GiantSteps_key/audio/10089 Jason Sparks - Close My Eyes feat. J. '
-                      'Little (Original Mix).mp3',
+        'Little (Original Mix).mp3',
         'keys_path': 'tests/resources/mir_datasets/GiantSteps_key/keys_gs+/10089 Jason Sparks - Close My Eyes feat. J. '
-                     'Little (Original Mix).txt',
+        'Little (Original Mix).txt',
         'metadata_path': 'tests/resources/mir_datasets/GiantSteps_key/meta/10089 Jason Sparks - Close My Eyes feat. J. '
-                         'Little (Original Mix).json',
+        'Little (Original Mix).json',
         'title': '10089 Jason Sparks - Close My Eyes feat. J. Little (Original Mix)',
         'track_id': '3',
     }
@@ -26,7 +26,7 @@ def test_track():
         'key': str,
         'genres': dict,
         'artists': list,
-        'tempo': int
+        'tempo': int,
     }
 
     run_track_tests(track, expected_attributes, expected_property_types)
@@ -42,35 +42,25 @@ def test_to_jams():
     data_home = 'tests/resources/mir_datasets/GiantSteps_key'
     track = giantsteps_key.Track('3', data_home=data_home)
     jam = track.to_jams()
-    assert (
-            jam['sandbox']['key'] == 'D major'
-    ), 'key does not match expected'
+    assert jam['sandbox']['key'] == 'D major', 'key does not match expected'
 
     assert (
-            jam['file_metadata']['title'] == '10089 Jason Sparks - Close My Eyes feat. J. Little (Original Mix)'
+        jam['file_metadata']['title']
+        == '10089 Jason Sparks - Close My Eyes feat. J. Little (Original Mix)'
     ), 'title does not match expected'
     sand_box = {
-        "artists": [
-          "Jason Sparks"
-        ],
-        "genres": {
-          "genres": [
-            "Breaks"
-          ],
-          "sub_genres": []
-        },
+        "artists": ["Jason Sparks"],
+        "genres": {"genres": ["Breaks"], "sub_genres": []},
         "tempo": 150,
-        "key": "D major"
+        "key": "D major",
     }
-    assert (
-            dict(jam['sandbox']) == sand_box
-    ), 'title does not match expected'
+    assert dict(jam['sandbox']) == sand_box, 'title does not match expected'
 
 
 def test_load_key():
     key_path = (
-            'tests/resources/mir_datasets/GiantSteps_key/keys_gs+/10089 Jason Sparks - Close My Eyes feat. J. ' +
-            'Little (Original Mix).txt'
+        'tests/resources/mir_datasets/GiantSteps_key/keys_gs+/10089 Jason Sparks - Close My Eyes feat. J. '
+        + 'Little (Original Mix).txt'
     )
     key_data = giantsteps_key.load_key(key_path)
 
@@ -83,8 +73,8 @@ def test_load_key():
 
 def test_load_meta():
     meta_path = (
-            'tests/resources/mir_datasets/GiantSteps_key/meta/10089 Jason Sparks - Close My Eyes feat. J. ' +
-            'Little (Original Mix).json'
+        'tests/resources/mir_datasets/GiantSteps_key/meta/10089 Jason Sparks - Close My Eyes feat. J. '
+        + 'Little (Original Mix).json'
     )
     genres = {'genres': ['Breaks'], 'sub_genres': []}
     artists = ['Jason Sparks']
