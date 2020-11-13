cf09db2fc5c8678d74486ab7b2ef39d08a12e716
Magdalena Fuentes <mgfuenteslujambio@gmail.com>
2020-11-13 18:26:49 +0100

    test top-level keys in index

--- tests/test_full_dataset.py
+++ tests/test_full_dataset.py
@@ -77,3 +77,18 @@ def test_load(skip_remote, dataset):
 
         jam = track.to_jams()
         assert jam.validate()
+
+
+def test_index(skip_remote, dataset):
+    if dataset is None:
+        pytest.skip()
+
+    okeys = ["tracks", "multitracks", "records"]
+
+    if "metadata" not in dataset._index.keys():
+        raise NotImplementedError("The top-level key 'metadata' is missing in the index")
+    if  "version" not in dataset._index.keys():
+        raise NotImplementedError("The top-level key 'version' is missing in the index")
+
+    if not any(key in dataset._index.keys() for key in okeys):
+        raise NotImplementedError("At least one of the optional top-level keys {} should be in the index".format(okeys))
