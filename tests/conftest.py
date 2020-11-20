37c3b60a04d4acf51ecff46a8cb096fb03a9c0e2
Marius Miron <miron.marius@gmail.com>
2020-11-20 16:48:32 +0100

    Marius/pytestemail (#337)
    * conftest email function
    conftest sessionfinish trigger
    
    * conftest add option to write txt logging the test results
    
    * extra print if folder does not exist
    
    * fix path name
    
    * add dataset
    
    Former-commit-id: 20094b10801a09a12b5d069b46cefe340c7648be

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
-
+import os
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -10,6 +10,7 @@ def pytest_addoption(parser):
     parser.addoption(
         "--skip-download", action="store_true", default=False, help="skip download step"
     )
+    parser.addoption("--report-file", type=str, default="", help="dataset to test locally")
 
 
 # @pytest.fixture(scope='session')
@@ -37,3 +38,42 @@ def test_dataset(request):
 @pytest.fixture(scope='session')
 def skip_download(request):
     return request.config.getoption('--skip-download')
+
+@pytest.fixture(scope='session')
+def report_file(request):
+    return request.config.getoption('--report-file')
+
+
+def pytest_sessionstart(session):
+    session.results = dict()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.when == 'call':
+        item.session.results[item] = result
+
+def pytest_sessionfinish(session, exitstatus):
+    if len(session.config.option.report_file)>0:
+        report = '\nTests: '+','.join(session.config.option.file_or_dir) + '\n'
+        report += 'Dataset: '+ str(session.config.option.dataset) + '\n'
+        report += 'Run status code: '+ str(exitstatus) + '\n'
+        report += 'Running time: '+str(session.config.option.durations_min)  + '\n'
+        passed_amount = sum(1 for result in session.results.values() if result.passed)
+        failed_amount = sum(1 for result in session.results.values() if result.failed)
+        report += 'There are {} passed and {} failed tests \n'.format(passed_amount,failed_amount)
+        print(report)
+        file_destination=session.config.option.report_file
+        if os.path.isdir(os.path.dirname(file_destination)):
+            if os.path.exists(file_destination):
+                append_write = 'a' # append if already exists
+            else:
+                append_write = 'w' # make a new file if not
+
+            with open(file_destination,append_write) as txtfile:
+                txtfile.write(report + '\n')
+        else:
+            print('Folder {} does not exist'.format(os.path.dirname(file_destination)))
