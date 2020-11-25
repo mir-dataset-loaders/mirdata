461b31a973f6c25e9738b908ef167b0543a7cb3f
Marius Miron <miron.marius@gmail.com>
2020-11-25 20:20:39 +0100

    added index for phenicx

--- scripts/make_phenicx_anechoic_index.py
+++ scripts/make_phenicx_anechoic_index.py
@@ -45,10 +45,11 @@ def make_dataset_index(data_path):
     }
     totalinstruments = [20, 39, 30, 10]
     ninstruments = [10, 10, 10, 8]
-    index = {}
+    index = {'version':1}
+    index['multitracks'] = {}
 
     for ip, piece in enumerate(pieces):
-        index[piece] = {}
+        index['multitracks'][piece] = {}
 
         audio_files = sorted(
             glob.glob(os.path.join(data_path, 'audio', piece, '*.wav'))
@@ -71,7 +72,29 @@ def make_dataset_index(data_path):
             len(set_instruments) == ninstruments[ip]
         ), 'some instruments are missing from the dataset'
 
+        index['multitracks'][piece]['tracks'] = {}
         for instrument in set_instruments:
+
+            #### add audios
+            instrument_audio_files = sorted(
+                glob.glob(os.path.join(data_path, 'audio', piece, instrument + '*.wav'))
+            )
+            assert (
+                len(instrument_audio_files) > 0
+            ), 'no audio has been found for {}'.format(instrument)
+
+            for i, audio_file in enumerate(instrument_audio_files):
+                audio_checksum = md5(
+                    os.path.join(
+                        data_path, 'audio', piece, os.path.basename(audio_file)
+                    )
+                )
+                source = os.path.basename(audio_file).replace('.wav', '')
+                index['multitracks'][piece]['tracks'][source] = (
+                    'audio/{}/{}'.format(piece, os.path.basename(audio_file)),
+                    audio_checksum,
+                )
+
             #### add scores
             assert os.path.exists(
                 os.path.join(
@@ -99,34 +122,14 @@ def make_dataset_index(data_path):
                 )
             )
 
-            index[piece]['score-' + instrument] = (
+            index['multitracks'][piece]['score-' + instrument] = (
                 'annotations/{}/{}.txt'.format(piece, instrument),
                 score_checksum,
             )
-            index[piece]['score_original-' + instrument] = (
+            index['multitracks'][piece]['score_original-' + instrument] = (
                 'annotations/{}/{}_o.txt'.format(piece, instrument),
                 score_original_checksum,
             )
-
-            #### add audios
-            instrument_audio_files = sorted(
-                glob.glob(os.path.join(data_path, 'audio', piece, instrument + '*.wav'))
-            )
-            assert (
-                len(instrument_audio_files) > 0
-            ), 'no audio has been found for {}'.format(instrument)
-
-            for i, audio_file in enumerate(instrument_audio_files):
-                audio_checksum = md5(
-                    os.path.join(
-                        data_path, 'audio', piece, os.path.basename(audio_file)
-                    )
-                )
-                source = os.path.basename(audio_file).replace('.wav', '')
-                index[piece]['audio-' + source] = (
-                    'audio/{}/{}'.format(piece, os.path.basename(audio_file)),
-                    audio_checksum,
-                )
 
     with open(DATASET_INDEX_PATH, 'w') as fhandle:
         json.dump(index, fhandle, indent=2)
