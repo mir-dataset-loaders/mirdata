91f95ee9f02476000853043461f91c9bacf11f8b
pedroramonedafranco <pppramonedafranco@hotmail.com>
2021-01-29 17:15:29 +0100

    commenting prototype

--- mirdata/datasets/haydn_op20.py
+++ mirdata/datasets/haydn_op20.py
@@ -1,54 +1,53 @@
 # -*- coding: utf-8 -*-
 """haydn op20 Dataset Loader
 
-.. admonition:: Dataset Info
-    :class: dropdown
+..
+    This dataset hqs a relative dependency Music21
+    admonition:: Dataset Info
+    This dataset accompanies the Master Thesis from Nestor Napoles. It is a manually-annotated corpus of harmonic
+    analysis in **harm syntax.
 
-    The haydn op20 Dataset includes 881 classical musical pieces across different styles from s.XVII to s.XX
-    annotated with single-key labels.
-
-    haydn op20 Dataset was created as part of:
-
-    .. code-block:: latex
-
-        Gómez, E. (2006). PhD Thesis. Tonal description of music audio signals.
-        Department of Information and Communication Technologies.
-
-    This dataset is mainly intended to assess the performance of computational key estimation algorithms in classical music.
-
-    2020 note: The audio is privates. If you don't have the original audio collection, you could create it from your private collection
-    because most of the recordings are well known. To this end, we provide musicbrainz metadata. Moreover, we have added the spectrum and
-    HPCP chromagram of each audio.
-
-    This dataset can be used with mirdata library:
-    https://github.com/mir-dataset-loaders/mirdata
-
-    Spectrum features have been computed as is shown here:
-    https://github.com/mir-dataset-loaders/mirdata-notebooks/blob/master/Tonality_classicalDB/ClassicalDB_spectrum_features.ipynb
-
-    HPCP chromagram has been computed as is shown here:
-    https://github.com/mir-dataset-loaders/mirdata-notebooks/blob/master/Tonality_classicalDB/ClassicalDB_HPCP_features.ipynb
-
-    Musicbrainz metadata has been computed as is shown here:
-    https://github.com/mir-dataset-loaders/mirdata-notebooks/blob/master/Tonality_classicalDB/ClassicalDB_musicbrainz_metadata.ipynb
-
+    The dataset contains the following scores:
+    Haydn, Joseph
+        1. E-flat major, op. 20 no. 1, Hob. III-31
+            I. Allegro moderato
+            II. Menuetto. Allegretto
+            III. Affettuoso e sostenuto
+            IV. Finale. Presto
+        2. C major, op. 20 no. 2, Hob. III-32
+            I. Moderato
+            II. Capriccio. Adagio
+            III. Menuetto. Allegretto
+            IV. Fuga a 4 soggetti
+        3. G minor, op. 20 no. 3, Hob. III-33
+            I. Allegro con spirito
+            II. Menuetto. Allegretto
+            III. Poco adagio
+            IV. Finale. Allegro molto
+        4. D major, op. 20 no. 4, Hob. III-34
+            I. Allegro di molto
+            II. Un poco adagio e affettuoso
+            III. Menuet alla Zingarese & Trio
+            IV. Presto e scherzando
+        5. F minor, op. 20 no. 5, Hob. III-35
+            I. Allegro moderato
+            II. Menuetto
+            III. Adagio
+            IV. Finale. Fuga a due soggetti
+        6. A major, op. 20 no. 6, Hob. III-36
+            I. Allegro di molto e scherzando
+            II. Adagio. Cantabile
+            III. Menuetto. Allegretto
+            IV. Fuga a 3 soggetti. Allegro
 """
 
-import csv
-import json
 import os
-from typing import Any, BinaryIO, Dict, Optional, TextIO, Tuple
-
-import librosa
-import numpy as np
+from typing import Any, BinaryIO, Dict, Optional, TextIO, Tuple, List
 
 from mirdata import core
 from mirdata import download_utils
-from mirdata import io
 from mirdata import jams_utils
 import music21
-
-import pysynth as ps
 
 
 BIBTEX = """
@@ -86,17 +85,19 @@ class Track(core.Track):
         track_id (str): track id of the track
 
     Attributes:
-        audio_path (str): track audio path
-        key_path (str): key annotation path
         title (str): title of the track
         track_id (str): track id
+        humdrum_annotated_path (str): path to humdrum annotated score
 
     Cached Properties:
         key (str): key annotation
-        spectrum (np.array): computed audio spectrum
-        hpcp (np.array): computed hpcp
-        musicbrainz_metadata (dict): MusicBrainz metadata
-
+        score (np.array): computed audio spectrum
+        keys (list): annotated local key
+        roman_numerals (list): MusicBrainz metadata
+        chords (list): annotated local key
+        duration (int): relative duration
+        midi_path (str): path to midi
+        score (music21.stream.Score): music21 score
     """
     def __init__(
         self,
@@ -120,27 +121,27 @@ class Track(core.Track):
         show_score(self.humdrum_annotated_path)
 
     @core.cached_property
-    def score(self):
+    def score(self) -> music21.stream.Score:
         return load_score(self.humdrum_annotated_path)
 
     @core.cached_property
-    def keys(self, resolution=28):
+    def keys(self, resolution=28) -> List[dict]:
         return load_key(self.humdrum_annotated_path, resolution)
 
     @core.cached_property
-    def roman_numerals(self, resolution=28):
+    def roman_numerals(self, resolution=28) -> List[dict]:
         return load_roman_numerals(self.humdrum_annotated_path, resolution)
 
     @core.cached_property
-    def chords(self, resolution=28):
+    def chords(self, resolution=28) -> List[dict]:
         return load_roman_numerals(self.humdrum_annotated_path, resolution)
 
     @core.cached_property
-    def duration(self):
+    def duration(self) -> int:
         return self.chords[-1]['time']
 
     @core.cached_property
-    def midi_path(self):
+    def midi_path(self) -> str:
         return load_midi_path(self.humdrum_annotated_path)
 
     def to_jams(self):
@@ -154,7 +155,6 @@ class Track(core.Track):
             metadata={
                 "duration": self.duration,
                 "title": self.title,
-                "score": self.score,
                 "keys": self.keys,
                 "roman_numerals": self.roman_numerals,
                 "midi_path": self.midi_path,
@@ -165,16 +165,41 @@ class Track(core.Track):
 
 
 def load_score(path):
+    """Load haydn op20 score from a file with music21 format (music21.stream.Score).
+
+            Args:
+                path: path to hrm annotations
+
+            Returns:
+                music21.stream.Score: score in music21 format
+
+    """
     score = music21.converter.parse(path, format='humdrum')
     return score
 
 
 def show_score(path):
+    """Show score in Musescore 3. Required to have Musescore 3 installed.
+
+            Args:
+                path: path to hrm annotations
+
+
+    """
     score = music21.converter.parse(path, format='humdrum')
     score.show()
 
 
 def load_key(path, resolution=28):
+    """Load haydn op20 key data from a file
+
+        Args:
+            path: path to hrm annotations
+
+        Returns:
+            List[dict]: musical key data and time
+
+    """
     score = music21.converter.parse(path, format='humdrum')
     rna = {rn.offset: rn for rn in list(score.flat.getElementsByClass('RomanNumeral'))}
     annotations = []
@@ -192,6 +217,15 @@ def load_key(path, resolution=28):
 
 
 def load_midi_path(path):
+    """Load path to midi file of haydn op20 musical piece
+
+            Args:
+                path: path to hrm annotations
+
+            Returns:
+                str: midi file path
+
+    """
     midi_path = os.path.splitext(path)[0] + '.midi'
     if not os.path.exists(midi_path):
         score = music21.converter.parse(path, format='humdrum')
@@ -201,6 +235,15 @@ def load_midi_path(path):
 
 
 def load_roman_numerals(path, resolution=28):
+    """Load haydn op20 roman numerals data from a file
+
+            Args:
+                path: path to hrm annotations
+
+            Returns:
+                List[dict]: musical roman numerals data and time
+
+    """
     score = music21.converter.parse(path, format='humdrum')
     rna = {rn.offset: rn for rn in list(score.flat.getElementsByClass('RomanNumeral'))}
     annotations = []
@@ -217,6 +260,15 @@ def load_roman_numerals(path, resolution=28):
 
 
 def load_chord(path, resolution=28):
+    """Load haydn op20 chords data from a file
+
+            `Args:
+                path: path to hrm annotations
+
+            Returns:
+                List[dict`]: musical roman numerals data and time
+
+    """
     score = music21.converter.parse(path, format='humdrum')
     rna = {rn.offset: rn for rn in list(score.flat.getElementsByClass('RomanNumeral'))}
     annotations = []
