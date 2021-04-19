769f3e6628632af164c74fd62902f06604558ff0
pedroramonedafranco <pppramonedafranco@hotmail.com>
2021-04-19 21:32:03 +0200

    update docs

--- mirdata/datasets/good_sounds.py
+++ mirdata/datasets/good_sounds.py
@@ -4,80 +4,26 @@
 .. admonition:: Dataset Info
     :class: dropdown
 
+    The Good-Sounds dataset is born of the collaboration between the Music Technology Group and Korg. Good-Sounds
+    [2, 16] is carried out recording a training dataset of single note excerpts including six classes of sounds per
+    studied instrument. Twelve different instruments are recorded, as is shown in Table 2. For each instrument,
+    the complete range of playable semitones is captured several times with various tonal characteristics. There
+    are two classes: Good and Bad sounds. Bad sounds are divided into five sub-classes, one for each musical dimension
+    stated by the expert musicians. Bad sounds are composed by examples of note recordings
+    that are intentionally badly played. The last class includes examples of note recordings that are considered to
+    be well played.
+
     This dataset was created in the context of the Pablo project, partially funded by KORG Inc. It contains monophonic
     recordings of two kind of exercises: single notes and scales.
-    It is organised in 4 entities:
-
-    Sounds
-
-
-    The entity containing the sounds annotations.
-
-        * id
-        * instrument: flute, cello, clarinet, trumpet, violin, sax_alto, sax_tenor, sax_baritone, sax_soprano, oboe, piccolo, bass
-        * note
-        * octave
-        * dynamics: for some sounds, the musical notation of the loudness level (p, mf, f..)
-        * recorded_at: recording date and time
-        * location: recording place
-        * player: the musician who recorded. For detailed information about the musicians contact us.
-        * bow_velocity: for some string instruments the velocity of the bow (slow, medieum, fast)
-        * bridge_position: for some string instruments the position of the bow (tasto, middle, ponticello)
-        * string: for some string instruments the number of the string in which the sound it's played (1: lowest in pitch)
-        * csv_file: used for creation of the DB
-        * csv_id: used for creation of the DB
-        * pack_filename: used for creation of the DB
-        * pack_id: used for creation of the DB
-        * attack: for single notes, manual annotation of the onset in samples.
-        * decay: for single notes, manual annotation of the decay in samples.
-        * sustain: for single notes, manual annotation of the beginnig of the sustained part in samples.
-        * release: for single notes, manual annotation of the beginnig of the release part in samples.
-        * offset: for single notes, manual annotation of the offset in samples
-        * reference: 1 if sound was used to create the models in the good-sounds project, 0 if not.
-        * klass: user generated tags of the tonal qualities of the sound. They also contain information about the exercise, that could be single note or scale.
-
-            * "good-sound":  good examples of single note
-            * "bad": bad example of one of the sound attributes defined in the project (please read the papers for a detailed explanation)
-            * "scale-good": good example of a one octave scale going up and down (15 notes). If the scale is minor a tagged "minor" is also available.
-            * "scale-bad": bad example scale of one of the sounds defined in the project. (15 notes up and down).
-            * Other tags regarding tonal characteristics are also available.
-
-                * comments: if any
-                * semitone: midi note
-                * pitch_reference: the reference pitch
-
-    Takes
+    The recordings were made in the Universitat Pompeu Fabra / Phonos recording studio by 15 different professional
+    musicians, all of them holding a music degree and having some expertise in teaching. 12 different instruments were
+    recorded using one or up to 4 different microphones (depending on the recording session). For all the instruments
+    the whole set of playable semitones in the instrument is recorded several times with different tonal characteristics.
+    Each note is recorded into a separate mono .flac audio file of 48kHz and 32 bits. The tonal characteristics are
+    explained both in the the following section and the related publication. The database is meant for organizing the
+    sounds in a handy way. It is organised in four different entities: sounds, takes, packs and ratings.
 
 
-    A sound can have several takes as some of them were recorded using different microphones at the same time. Each take has an associated audio file.
-        - id
-        - microphone
-        - filename: the name of the associated audio file
-        - original_filename:
-        - freesound_id: for some sounds uploaded to freesound.org
-        - sound_id: the id of the sound in the DB
-        - goodsound_id: for some of the sounds available in good-sounds.org
-
-    Packs
-
-
-    A pack is a group of sounds from the same recording session. The audio files are organised in the sound_files directory in subfolders with the pack name to which they belong.
-        - id
-        - name
-        - description
-
-
-    Ratings
-
-
-    Some musicians self-rated their performance in a 0-10 goodness scale for the user evaluation of the first project prototype. Please read the paper for more detailed information.
-        - id
-        - mark: the rate or score.
-        - type: the klass of the sound. Related to the tags of the sound.
-        - created_at
-        - comments
-        - sound_id
-        - rater: the musician who rated the sound.
 
 
 """
@@ -140,12 +86,68 @@ class Track(core.Track):
         audio_path (str): Path to the audio file
 
     Cached Properties:
-        ratings_info (dict):  Some musicians self-rated some sounds in a 0-10 goodness scale for the user evaluation of the first
-            project prototype. Please read the paper for more detailed information.
-        pack_info (dict):
-        sound_info (dict): The entity containing the sounds annotations.
-        take_info (dict): A sound can have several takes as some of them were recorded using different microphones at the same
+        ratings_info (dict):  A dictionary containing the entity Ratings.
+        Some musicians self-rated their performance in a 0-10 goodness scale for the user evaluation of the first project prototype. Please read the paper for more detailed information.
+            - id
+            - mark: the rate or score.
+            - type: the klass of the sound. Related to the tags of the sound.
+            - created_at
+            - comments
+            - sound_id
+            - rater: the musician who rated the sound.
+
+        pack_info (dict): A dictionary containing the entity Pack.
+            A pack is a group of sounds from the same recording session. The audio files are organised in the sound_files directory in subfolders with the pack name to which they belong.
+                - id
+                - name
+                - description
+
+        sound_info (dict): A dictionary containing the entity Sound.
+            A sound can have several takes as some of them were recorded using different microphones at the same time. Each take has an associated audio file.
+                - id
+                - microphone
+                - filename: the name of the associated audio file
+                - original_filename:
+                - freesound_id: for some sounds uploaded to freesound.org
+                - sound_id: the id of the sound in the DB
+                - goodsound_id: for some of the sounds available in good-sounds.org
+
+        take_info (dict): A dictionary containing the entity Take.
+            A sound can have several takes as some of them were recorded using different microphones at the same
             time. Each take has an associated audio file.
+            The annotations:
+            * id
+            * instrument: flute, cello, clarinet, trumpet, violin, sax_alto, sax_tenor, sax_baritone, sax_soprano, oboe, piccolo, bass
+            * note
+            * octave
+            * dynamics: for some sounds, the musical notation of the loudness level (p, mf, f..)
+            * recorded_at: recording date and time
+            * location: recording place
+            * player: the musician who recorded. For detailed information about the musicians contact us.
+            * bow_velocity: for some string instruments the velocity of the bow (slow, medieum, fast)
+            * bridge_position: for some string instruments the position of the bow (tasto, middle, ponticello)
+            * string: for some string instruments the number of the string in which the sound it's played (1: lowest in pitch)
+            * csv_file: used for creation of the DB
+            * csv_id: used for creation of the DB
+            * pack_filename: used for creation of the DB
+            * pack_id: used for creation of the DB
+            * attack: for single notes, manual annotation of the onset in samples.
+            * decay: for single notes, manual annotation of the decay in samples.
+            * sustain: for single notes, manual annotation of the beginnig of the sustained part in samples.
+            * release: for single notes, manual annotation of the beginnig of the release part in samples.
+            * offset: for single notes, manual annotation of the offset in samples
+            * reference: 1 if sound was used to create the models in the good-sounds project, 0 if not.
+            * klass: user generated tags of the tonal qualities of the sound. They also contain information about the exercise, that could be single note or scale.
+
+                * "good-sound":  good examples of single note
+                * "bad": bad example of one of the sound attributes defined in the project (please read the papers for a detailed explanation)
+                * "scale-good": good example of a one octave scale going up and down (15 notes). If the scale is minor a tagged "minor" is also available.
+                * "scale-bad": bad example scale of one of the sounds defined in the project. (15 notes up and down).
+                * Other tags regarding tonal characteristics are also available.
+
+                    * comments: if any
+                    * semitone: midi note
+                    * pitch_reference: the reference pitch
     """
 
     def __init__(
