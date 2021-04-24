b2cd1ab96768dfb1f867ca22ecb023d68b1293ad
pedroramonedafranco <pppramonedafranco@hotmail.com>
2021-04-24 16:28:37 +0200

    add files

---
+++ scripts/make_mtg_jamendo_autotagging_moodtheme.py
@@ -0,0 +1,42 @@
+import argparse
+import hashlib
+import json
+import os
+from mirdata.validate import md5
+
+
+beatport_key_INDEX_PATH = '../mirdata/datasets/indexes/mtg_jamendo_autotagging_moodtheme.json'
+
+
+def make_beatport_key_index(data_path):
+    audio_dir = os.path.join(data_path, 'audio')
+    beatport_key_index = {
+        'version': '1.0.0',
+        'tracks': {}
+    }
+    for track_id, ann_dir in enumerate(sorted(os.listdir(key_dir))):
+        if '.txt' in ann_dir:
+            codec = '.mp3'
+            audio_path = os.path.join(audio_dir, ann_dir.replace('.txt', codec))
+            chord_path = os.path.join(key_dir, ann_dir)
+            meta_path = os.path.join(meta_dir, ann_dir.replace('.txt', '.json'))
+            if not os.path.exists(meta_path):
+                meta = (None, None)
+            else:
+                meta = (meta_path.replace(data_path + '/', ''), md5(meta_path))
+
+            beatport_key_index['tracks'][track_id] = {
+                'audio': (audio_path.replace(data_path + '/', ''), md5(audio_path)),
+            }
+    with open(beatport_key_INDEX_PATH, 'w') as fhandle:
+        json.dump(beatport_key_index, fhandle, indent=2)
+
+
+def main(args):
+    make_beatport_key_index(args.beatport_key_data_path)
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(description='Make beatport_key index file.')
+    PARSER.add_argument('beatport_key_data_path', type=str, help='Path to beatport_key data folder.')
+    main(PARSER.parse_args())
