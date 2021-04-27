94dc5b54eae7d36007260bf80b1aa5d0efe22129
pedroramonedafranco <pppramonedafranco@hotmail.com>
2021-04-28 00:41:10 +0200

    fix docstring

--- mirdata/datasets/da_tacos.py
+++ mirdata/datasets/da_tacos.py
@@ -46,7 +46,6 @@
     CSI research can reach larger audiences.
 
     Pre-extracted features
-    ^^^^^^^^^^^^^^^^^^^^^^
 
     The list of features included in Da-TACOS can be seen below. All the features are extracted with acoss 
     repository that uses open-source feature extraction libraries such as Essentia, LibROSA, and Madmom.
