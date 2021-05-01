4b694bf2347b2673a6e49877775da077b27c8a16
Rachel Bittner <rachelbittner@spotify.com>
2021-05-01 23:16:52 +0200

    add note annotations for ikala and medleydb pitch

--- scripts/make_medleydb_pitch_index.py
+++ scripts/make_medleydb_pitch_index.py
@@ -3,28 +3,9 @@ import hashlib
 import json
 import os
 
+from mirdata.validate import md5
 
 MEDLEYDB_PITCH_INDEX_PATH = "mirdata/datasets/indexes/medleydb_pitch_index_{}.json"
-
-
-def md5(file_path):
-    """Get md5 hash of a file.
-
-    Parameters
-    ----------
-    file_path: str
-        File path.
-
-    Returns
-    -------
-    md5_hash: str
-        md5 hash of data in file_path
-    """
-    hash_md5 = hashlib.md5()
-    with open(file_path, "rb") as fhandle:
-        for chunk in iter(lambda: fhandle.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
 
 
 def strip_first_dir(full_path):
@@ -32,35 +13,42 @@ def strip_first_dir(full_path):
 
 
 def make_medleydb_pitch_index(data_path, version):
-    assert version in [2.0, 3.0], "invalid version"
+    assert version in ["2.0", "3.0"], "invalid version"
 
-    metadata_path = os.path.join(
-        data_path, "MedleyDB-Pitch", "medleydb_pitch_metadata.json"
-    )
+    metadata_path = os.path.join(data_path, "medleydb_pitch_metadata.json")
     with open(metadata_path, "r") as fhandle:
         metadata = json.load(fhandle)
 
     pitch_index = {}
     pitch_index["tracks"] = {}
     for trackid in metadata.keys():
-        audio_path = os.path.join(data_path, metadata[trackid]["audio_path"])
+        audio_rel = strip_first_dir(metadata[trackid]["audio_path"])
+        pitch_rel = strip_first_dir(metadata[trackid]["pitch_path"])
+        audio_path = os.path.join(data_path, audio_rel)
         audio_checksum = md5(audio_path)
-        local_pitch_path = os.path.join(data_path, metadata[trackid]["pitch_path"])
+        local_pitch_path = os.path.join(data_path, pitch_rel)
         pitch_checksum = md5(local_pitch_path)
 
         fullid = os.path.basename(audio_path).split(".")[0]
         pitch_index["tracks"][fullid] = {
-            "audio": (strip_first_dir(metadata[trackid]["audio_path"]), audio_checksum),
-            "pitch": (strip_first_dir(metadata[trackid]["pitch_path"]), pitch_checksum),
+            "audio": (audio_rel, audio_checksum),
+            "pitch": (pitch_rel, pitch_checksum),
         }
-        if version == 3.0:
-            note_path = "medleydb-pitch-pyin-notes/{}_vamp_pyin_pyin_notes.csv".format(
+        if version == "3.0":
+            note_rel = "medleydb-pitch-pyin-notes/{}_vamp_pyin_pyin_notes.csv".format(
                 fullid
             )
-            pitch_index["tracks"][fullid]["notes_pyin"] = (
-                note_path,
-                md5(os.path.join(data_path)),
-            )
+            note_full = os.path.join(data_path, note_rel)
+
+            if os.path.exists(note_full):
+                pitch_index["tracks"][fullid]["notes_pyin"] = (
+                    note_rel,
+                    md5(note_full),
+                )
+            else:
+                print(note_rel)
+                pitch_index["tracks"][fullid]["notes_pyin"] = (None, None)
+
     pitch_index["version"] = version
     pitch_index["metadata"] = {
         "medleydb_pitch_metadata": ("medleydb_pitch_metadata.json", md5(metadata_path))
