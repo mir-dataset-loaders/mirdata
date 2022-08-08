326a1df5108d3783182465c38079890803aa3811
dave-foster <44112885+dave-foster@users.noreply.github.com>
2022-08-08 11:30:58 +0100

    Still trying

--- mirdata/datasets/filosax.py
+++ mirdata/datasets/filosax.py
@@ -252,8 +252,8 @@ class Track(core.Track):
         note_path = "" if self.annotation_path == None else self.annotation_path
         if note_path == "":
             print("Note data only available for Sax tracks.")
-            note_dict = {}
-            return [Note(n) for n in note_dict]
+            #note_dict = {}
+            return []
         else:
             with open(note_path) as fhandle:
                 note_dict = json.load(fhandle)["notes"]
