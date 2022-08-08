0827351160ccbf8042d57a7462319ff14a68ce9e
dave-foster <44112885+dave-foster@users.noreply.github.com>
2022-08-08 11:12:18 +0100

    Formatting fix

--- mirdata/datasets/filosax.py
+++ mirdata/datasets/filosax.py
@@ -251,13 +251,13 @@ class Track(core.Track):
         """
         note_path = "" if self.annotation_path == None else self.annotation_path
         if note_path == "":
-             print("Note data only available for Sax tracks.")
-             note_dict = {}
-             return [Note(n) for n in note_dict]
-         else:
-             with open(note_path) as fhandle:
-                 note_dict = json.load(fhandle)["notes"]
-         return [Note(n) for n in note_dict]
+            print("Note data only available for Sax tracks.")
+            note_dict = {}
+            return [Note(n) for n in note_dict]
+        else:
+            with open(note_path) as fhandle:
+                note_dict = json.load(fhandle)["notes"]
+        return [Note(n) for n in note_dict]
 
     @property
     def audio(self) -> Optional[Tuple[np.ndarray, float]]:
