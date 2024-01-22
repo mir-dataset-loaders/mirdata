e18dea59d705bde53a88a90ed72f0c1a162a2e32
Tanmay Khandelwal <36072284+rythmm24@users.noreply.github.com>
2024-01-22 12:55:20 -0500

    Adding loader for cuidado

---
+++ scripts/make_cuidado_index.py
@@ -0,0 +1,51 @@
+import argparse
+import hashlib
+import json
+import os
+import glob
+from mirdata.validate import md5
+
+CUIDADO_RHYTHM_INDEX_PATH = "../mirdata/datasets/indexes/cuidado_full_index_1.0.json"
+
+def make_cuidado_rhythm_index(dataset_data_path):
+    cmr_index = {
+        "version": "1.0",
+        "tracks": {},
+    }
+    
+    dataset_folder_name = "C_1.0"  # Update this folder name if needed
+    for root, dirs, files in os.walk(dataset_data_path):
+        for filename in files:
+            if filename.endswith(".flac"):
+                subfolder = os.path.basename(root)
+                idx = filename.split(".")[0]
+                cmr_index["tracks"][idx] = {
+                    "audio": (
+                        os.path.join(dataset_folder_name, "audio", filename),
+                        md5(os.path.join(root, filename)),
+                    ),
+                    "beats": (
+                        os.path.join(dataset_folder_name, "annotations", "beats", filename.replace(".flac", ".beats")),
+                        md5(os.path.join(dataset_data_path, "annotations", "beats",  filename.replace(".flac", ".beats"))),
+                    ),
+                    "tempo": (
+                        os.path.join(dataset_folder_name, "annotations", "tempo",  filename.replace(".flac", ".bpm")),
+                        md5(os.path.join(dataset_data_path, "annotations", "tempo",  filename.replace(".flac", ".bpm"))),
+                    )
+                }
+
+    with open(CUIDADO_RHYTHM_INDEX_PATH, "w") as fhandle:
+        json.dump(cmr_index, fhandle, indent=2)
+
+def main(args):
+    print("creating index...")
+    make_cuidado_rhythm_index(args.dataset_data_path)
+    print("done!")
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser(description="Make Cuidado Rhythm index file.")
+    PARSER.add_argument(
+        "dataset_data_path", type=str, help="Path to Cuidado Rhythm data folder."
+    )
+
+    main(PARSER.parse_args())
