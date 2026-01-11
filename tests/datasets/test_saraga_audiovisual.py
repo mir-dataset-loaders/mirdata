4676a2647e991907c5993214810fe23ed2abcad4
vivekvjyn <vivek.vijayan01@estudiant.upf.edu>
2026-01-11 11:25:47 +0530

    writing tests

--- tests/datasets/test_saraga_audiovisual.py
+++ tests/datasets/test_saraga_audiovisual.py
@@ -106,179 +106,105 @@ def test_track():
     assert audio.shape[0] == 2
 
 
-def test_load_tonic():
-    data_home = "tests/resources/mir_datasets/compmusic_iamms"
-    dataset = compmusic_iamms.Dataset(data_home, version="test")
-    track = dataset.track("0_ALB_Bhairavi1_70albmgpnganesan")
-    tonic_path = track.tonic_path
-    tonic_finetuned_path = track.tonic_finetuned_path
-    parsed_tonic = compmusic_iamms.load_tonic(tonic_path)
-    parsed_tonic_finetuned = compmusic_iamms.load_tonic(tonic_finetuned_path)
-    assert parsed_tonic == 134.000
-    assert parsed_tonic_finetuned == 134.000
-    assert compmusic_iamms.load_tonic(None) is None
-
-
-def test_load_pitch():
-    data_home = "tests/resources/mir_datasets/compmusic_iamms"
-    dataset = compmusic_iamms.Dataset(data_home, version="test")
-    track = dataset.track("0_ALB_Bhairavi1_70albmgpnganesan")
-    pitch_path = track.pitch_path
-    pitch_finetuned_path = track.pitch_finetuned_path
-    parsed_pitch = compmusic_iamms.load_pitch(pitch_path)
-    parsed_pitch_finetuned = compmusic_iamms.load_pitch(pitch_finetuned_path)
-
-    # Check types
-    assert type(parsed_pitch) == annotations.F0Data
-    assert type(parsed_pitch.times) is np.ndarray
-    assert type(parsed_pitch.frequencies) is np.ndarray
-    assert type(parsed_pitch.voicing) is np.ndarray
-    assert type(parsed_pitch_finetuned) == annotations.F0Data
-    assert type(parsed_pitch_finetuned.times) is np.ndarray
-    assert type(parsed_pitch_finetuned.frequencies) is np.ndarray
-    assert type(parsed_pitch_finetuned.voicing) is np.ndarray
-
-    # Check values
-    assert np.array_equal(
-        parsed_pitch.times,
-        np.array(
-            [
-                0.000000000000000000e00,
-                2.902494331065759697e-03,
-                5.804988662131519393e-03,
-                8.707482993197278656e-03,
-            ]
-        ),
-    )
-    assert np.array_equal(
-        parsed_pitch_finetuned.times,
-        np.array(
-            [
-                0.000000000000000000e00,
-                2.902494331065759697e-03,
-                5.804988662131519393e-03,
-                8.707482993197278656e-03,
-            ]
-        ),
-    )
-    assert np.array_equal(
-        parsed_pitch.frequencies,
-        np.array(
-            [
-                0.0000000,
-                100.1200000,
-                200.2300000,
-                300.3400000,
-            ]
-        ),
-    )
-    assert np.array_equal(
-        parsed_pitch_finetuned.frequencies,
-        np.array(
-            [
-                0.0000000,
-                100.1200000,
-                200.2300000,
-                300.3400000,
-            ]
-        ),
-    )
-    assert np.array_equal(parsed_pitch.voicing, np.array([0.0, 1.0, 1.0, 1.0]))
-    assert np.array_equal(
-        parsed_pitch_finetuned.voicing, np.array([0.0, 1.0, 1.0, 1.0])
-    )
-
-    assert compmusic_iamms.load_pitch(None) is None
-
-
-def test_load_sections():
-    data_home = "tests/resources/mir_datasets/compmusic_iamms"
-    dataset = compmusic_iamms.Dataset(data_home, version="test")
-    track = dataset.track("0_ALB_Bhairavi1_70albmgpnganesan")
-    sections_path = track.sections_path
-    sections_finetuned_path = track.sections_finetuned_path
-    parsed_sections = compmusic_iamms.load_sections(sections_path)
-    parsed_sections_finetuned = compmusic_iamms.load_sections(sections_finetuned_path)
-
-    # Check types
-    assert type(parsed_sections) == annotations.SectionData
-    assert type(parsed_sections.intervals) is np.ndarray
-    assert type(parsed_sections.labels) is list
-    assert type(parsed_sections_finetuned) == annotations.SectionData
-    assert type(parsed_sections_finetuned.intervals) is np.ndarray
-    assert type(parsed_sections_finetuned.labels) is list
-
-    # Check values
-    assert np.array_equal(
-        parsed_sections.intervals[:, 0],
-        np.array([0.835918, 89.083356, 96.636735]),
-    )
-    assert np.array_equal(
-        parsed_sections.intervals[:, 1],
-        np.array([2.565805, 90.661224, 98.086803]),
-    )
-    assert parsed_sections.labels == ["1000", "2000", "3000"]
-    assert np.array_equal(
-        parsed_sections_finetuned.intervals[:, 0],
-        np.array([0.835918, 89.083356, 96.636735]),
-    )
-    assert np.array_equal(
-        parsed_sections_finetuned.intervals[:, 1],
-        np.array([2.565805, 90.661224, 98.086803]),
-    )
-    assert parsed_sections_finetuned.labels == ["1000", "2000", "3000"]
-
-    assert compmusic_iamms.load_sections(None) is None
-
-
-def test_load_nyas():
-    data_home = "tests/resources/mir_datasets/compmusic_iamms"
-    dataset = compmusic_iamms.Dataset(data_home, version="test")
-    track = dataset.track("0_ALB_Bhairavi1_70albmgpnganesan")
-    nyas_path = track.nyas_path
-    parsed_nyas = compmusic_iamms.load_nyas(nyas_path)
-
-    # Check types
-    assert type(parsed_nyas) is annotations.EventData
-    assert type(parsed_nyas.intervals) is np.ndarray
-    assert type(parsed_nyas.events) is list
-
-    # Check values
-    assert np.array_equal(
-        parsed_nyas.intervals,
-        np.array(
-            [
-                [2.11882, 3.37560],
-                [5.50023, 5.65116],
-                [5.87465, 5.99075],
-            ]
-        ),
-    )
-    assert parsed_nyas.events == ["nyas", "nyas", "nyas"]
-    assert compmusic_iamms.load_nyas(None) is None
-
-
 def test_load_audio():
     data_home = "tests/resources/mir_datasets/saraga_audiovisual"
-    dataset = compmusic_iamms.Dataset(data_home, version="test")
+    dataset = saraga_audiovisual.Dataset(data_home, version="test")
     track = dataset.track("0_Devi_Pavane")
     audio_path = track.audio_path
-    audio, sr = compmusic_iamms.load_audio(audio_path)
+    audio, sr = saraga_audiovisual.load_audio(audio_path)
 
     assert sr == 44100
     assert type(audio) == np.ndarray
     assert audio.shape[0] == 2
 
-    assert compmusic_iamms.load_audio(None) is None
+    assert saraga_audiovisual.load_audio(None) is None
 
 def test_load_video():
     data_home = "tests/resources/mir_datasets/saraga_audiovisual"
-    dataset = compmusic_iamms.Dataset(data_home, version="test")
+    dataset = saraga_audiovisual.Dataset(data_home, version="test")
     track = dataset.track("0_Devi_Pavane")
     video_path = track.video_path
-    video, fps = compmusic_iamms.load_video(video_path)
+    video, fps = saraga_audiovisual.load_video(video_path)
 
     assert type(video) == np.ndarray
     assert type(fps) == float
 
-    assert compmusic_iamms.load_video(None) is None
+    assert saraga_audiovisual.load_video(None) is None
+
+def test_load_metadtata(metadata_path):
+    data_home = "tests/resources/mir_datasets/saraga_audiovisual"
+    dataset = saraga_audiovisual.Dataset(data_home, version="test")
+    track = dataset.track("0_Devi_Pavane")
+    metadata_path = track.metadata_path
+    parsed_metadata = saraga_audiovisual.load_metadata(metadata_path)
+
+
+    assert parsed_metadata["mbid"] == "bf197cad-30d5-478c-9eae-b0897eef7a4f"
+    assert parsed_metadata["title"] == "Devi Pavane"
+    assert parsed_metadata["length"] == 2599.0333333333333
+    assert parsed_metadata["artists"] == [
+        {
+            "mbid": "bf197cad-30d5-478c-9eae-b0897eef7a4f",
+            "name": "Abhiram Bode",
+            "instrument": {
+                "mbid": "d92884b7-ee0c-46d5-96f3-918196ba8c5b",
+                "name": "Voice"
+            },
+            "lead": True,
+            "attributes": "lead vocals"
+        },
+        {
+            "mbid": "9b8912a2-afa1-4a14-8c5f-5225421a2bb5",
+            "name": "Sivateja",
+            "instrument": {
+                "mbid": "089f123c-0f7d-4105-a64e-49de81ca8fa4",
+                "name": "Violin"
+            },
+            "lead": False,
+            "attributes": ""
+        },
+        {
+            "mbid": "b5b3f876-cd38-4d50-b1ad-ac9e18ad73d4",
+            "name": "Adudurai Guruprasad",
+            "instrument": {
+                "mbid": "f689271c-37bc-4c49-92a3-a14b15ee5d0e",
+                "name": "Mridangam"
+            },
+            "lead": False,
+            "attributes": ""
+        }
+    ]
+    assert parsed_metadata["raaga"] == [
+        {
+            "uuid": "",
+            "name": "Saveri"
+        }
+    ]
+    assert parsed_metadata["taala"] == [
+        {
+            "uuid": "",
+            "name": "Adi"
+        }
+    ]
+    assert parsed_metadata["form"] == []
+    assert parsed_metadata["work"] == []
+    assert parsed_metadata["concert"] == []
+    assert parsed_metadata["album_artists"] == []
+
+def test_load_gesture():
+    data_home = "tests/resources/mir_datasets/saraga_audiovisual"
+    dataset = saraga_audiovisual.Dataset(data_home, version="test")
+    track = dataset.track("0_Devi_Pavane")
+    track = dataset.track("0_Devi_Pavane")
+    keypoints_path = track.keypoints_path['singer']
+    scores_path = scores.keypoints_path['singer']
+    gesture = saraga_audiovisual.load_gesture(keypoints_path, scores_path)
+
+    assert gesture.keypoints == np.array([
+        [100, 200],
+        [200, 400]
+    ])
+
+    assert gesture.score == np.array([
+        [1, 0.5]
+    ])
