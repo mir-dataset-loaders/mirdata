7285c50cc4ab80cde6c1ee105b3ee22cf243934b
Yujin <yk3281@nyu.edu>
2026-07-24 12:35:15 -0400

    black formatting

--- tests/datasets/test_vienna4x22.py
+++ tests/datasets/test_vienna4x22.py
@@ -31,7 +31,9 @@ def test_track():
         "piece": "Chopin_op10_no3",
         "pianist_id": "01",
         "alignment_quality": "manual",
-        "audio_path": os.path.join(DATA_HOME, "audio/Chopin_Etude/Chopin_op10_no3_p01.wav"),
+        "audio_path": os.path.join(
+            DATA_HOME, "audio/Chopin_Etude/Chopin_op10_no3_p01.wav"
+        ),
         "score_path": os.path.join(DATA_HOME, "musicxml/Chopin_op10_no3.musicxml"),
         "performance_path": os.path.join(DATA_HOME, "midi/Chopin_op10_no3_p01.mid"),
         "match_path": os.path.join(DATA_HOME, "match/Chopin_op10_no3_p01.match"),
